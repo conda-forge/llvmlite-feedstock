8bbfb393d3431388cd4c0582f8fe76f22974208b
step21 <step21@devtal.de>
2020-07-01 01:45:49 +0200

    fixing manual cmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,19 @@
 @rem Let CMake know about the LLVM install path, for find_package()
 set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
-set ZLIB_ROOT_DIR=%LIBRARY_PREFIX%
-set LLVM_CONFIG=%LIBRARY_PREFIX%\bin\llvm_config.exe
-set CMAKE_GENERATOR=Visual Studio 15 2017 Win64
-set ZLIB_NAME=zlib
-set ZLIB_LIBRARY_PATH=%LIBRARY_LIB%
-set ZLIB_INCLUDE=%LIBRARY_INC%
-SET LIBPATH=%LIBRARY_LIB%
-set LLVM_ENABLE_ZLIB=ON
-set PREFIX=%LIBRARY_PREFIX%
-set "LDFLAGS=%LDFLAGS% /LIBPATH:%LIBRARY_LIB%"
+
+@rem set ZLIB_ROOT_DIR=%LIBRARY_PREFIX%
+@rem set LLVM_CONFIG=%LIBRARY_PREFIX%\bin\llvm_config.exe
+@rem set CMAKE_GENERATOR=Visual Studio 15 2017 Win64
+@rem set ZLIB_NAME=zlib
+@rem set ZLIB_LIBRARY_PATH=%LIBRARY_LIB%
+@rem set ZLIB_INCLUDE=%LIBRARY_INC%
+@rem SET LIBPATH=%LIBRARY_LIB%
+@rem set LLVM_ENABLE_ZLIB=ON
+@rem set PREFIX=%LIBRARY_PREFIX%
+@rem set "LDFLAGS=%LDFLAGS% /LIBPATH:%LIBRARY_LIB%"
 
 @rem set verbose mode for debugging
-set VERBOSE=1
+@rem set VERBOSE=1
 
 @rem Ensure there are no build leftovers (CMake can complain)
 if exist ffi\build rmdir /S /Q ffi\build
@@ -29,3 +30,7 @@ if errorlevel 1 exit 1
 
 %PYTHON% runtests.py
 if errorlevel 1 exit 1
+
+@rem Let CMake know about the LLVM install path, for find_package()
+set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
+
