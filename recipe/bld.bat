2f1616980e745824523abc0ddb6f9f35404a5735
step21 <step21@devtal.de>
2020-07-01 11:57:24 +0200

    more cleanup and testing new error

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,22 +3,19 @@ set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
 
 @rem set ZLIB_ROOT_DIR=%LIBRARY_PREFIX%
 @rem set LLVM_CONFIG=%LIBRARY_PREFIX%\bin\llvm_config.exe
-@rem set CMAKE_GENERATOR=Visual Studio 15 2017 Win64
+set CMAKE_GENERATOR=Visual Studio 15 2017 Win64
 @rem set ZLIB_NAME=zlib
 @rem set ZLIB_LIBRARY_PATH=%LIBRARY_LIB%
 @rem set ZLIB_INCLUDE=%LIBRARY_INC%
-@rem SET LIBPATH=%LIBRARY_LIB%
 @rem set LLVM_ENABLE_ZLIB=ON
 @rem set PREFIX=%LIBRARY_PREFIX%
 set "LDFLAGS=%LDFLAGS% /LIBPATH:%LIBRARY_LIB%"
 
 @rem set verbose mode for debugging
-@rem set VERBOSE=1
+set VERBOSE=1
 
 @rem Ensure there are no build leftovers (CMake can complain)
 if exist ffi\build rmdir /S /Q ffi\build
-
-@rem llvm-config.exe --libs
 
 %PYTHON% -S setup.py install
 if errorlevel 1 exit 1
