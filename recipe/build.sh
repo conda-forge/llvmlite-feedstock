67353febb46392413f3e3cc6d37183c7ede3975a
Timo Strunk <timo.strunk@gmail.com>
2025-02-13 09:44:53 +0100

    Switched to static linking of llvm for linux-64 and osx-*

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,11 @@ set -x
 export PYTHONNOUSERSITE=1
 
 export LLVMLITE_USE_CMAKE=1
-export LLVMLITE_SHARED=1
+if [[ "${target_platform}" == "linux-ppc64le" ]]; then
+    export LLVMLITE_SHARED=1
+else
+    export LLVMLITE_SHARED=0
+fi
 
 if [[ "${target_platform}" == osx-* ]]; then
     # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
