03326d4ac9eb494d308e4fdb4d52e9412143f6ab
Isuru Fernando <isuruf@gmail.com>
2025-02-27 09:11:37 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,8 @@ export LLVMLITE_SHARED=0
 if [[ "${target_platform}" == osx-* ]]; then
     # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
     CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+elif [[ "${target_platform}" == linux-ppc64le ]]; then
+    CXXFLAGS="${CXXFLAGS} -fplt"
 fi
 
 $PYTHON setup.py build --force
