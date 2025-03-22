c6f123a5177fe09208ccce90d55e4e6c5c7d46e2
jakirkham <jakirkham@gmail.com>
2025-03-21 19:21:25 -0700

    Match common `-fno-plt` flag removal on PPC

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,12 @@ if [[ "${target_platform}" == osx-* ]]; then
     # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
     CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 elif [[ "${target_platform}" == linux-ppc64le ]]; then
-    CXXFLAGS="${CXXFLAGS} -fplt"
+    # Taken from llvmdev's recipe
+    # https://github.com/conda-forge/llvmdev-feedstock/blob/8c2c0f2db9db1fdf12289381dcee4e2d9a2e5fec/recipe/build.sh#L29-L33
+    # disable `-fno-plt` due to some GCC bug causing linker errors, see
+    # https://github.com/llvm/llvm-project/issues/51205
+    CFLAGS="$(echo $CFLAGS | sed 's/-fno-plt //g')"
+    CXXFLAGS="$(echo $CXXFLAGS | sed 's/-fno-plt //g')"
 fi
 
 $PYTHON setup.py build --force
